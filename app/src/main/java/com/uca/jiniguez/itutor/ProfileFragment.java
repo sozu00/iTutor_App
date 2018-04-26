9777c65a1968477afc2b55d91c609e90609787e9
sozu00 <inhiguez.jesus@gmail.com>
2018-04-26 20:10:35 +0200

    pestaña profile v0

--- app/src/main/java/com/uca/jiniguez/itutor/ProfileFragment.java
+++ app/src/main/java/com/uca/jiniguez/itutor/ProfileFragment.java
@@ -1,12 +1,16 @@
 package com.uca.jiniguez.itutor;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -14,7 +18,7 @@ import android.widget.TextView;
  */
 public class ProfileFragment extends Fragment {
 
-
+    View v;
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -23,7 +27,24 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        v =  inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+        TextView txt = v.findViewById(R.id.editText);
+        ListView listView = v.findViewById(R.id.SkillsList);
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Context context = v.getContext();
+                CharSequence text = "Hello toast!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                return true;
+            }
+        });
+        return v;
     }
 
 }
